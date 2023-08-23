36b8b2838ca7223ff96065b1ca6ac1806caa71b8
sethupathiraja007 <sethupathirajae1999@gmail.com>
2023-08-23 11:05:24 +0530

    23 th aug- commit all

--- calculator.js
+++ calculator.js
@@ -1,18 +1,26 @@
+// Get the input screen element by its ID
 let inputscreen = document.getElementById("input");
 
+// Function to display numbers and operators on the input screen
 function display(num) {
   inputscreen.value += num;
 }
+
+// Function to perform calculation
 function calculate() {
   try {
     inputscreen.value = eval(inputscreen.value);
   } catch (err) {
-    alert("invalid input");
+    alert("Invalid input");
   }
 }
+
+// Function to clear the input
 function Clear() {
-  inputscreen.value = inputscreen.value.slice(0, 0);
+  inputscreen.value = "";
 }
+
+// Function to delete the last character
 function del() {
   inputscreen.value = inputscreen.value.slice(0, -1);
 }
